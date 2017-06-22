8d90c00ba6ffcaf277245539af40f1e3a2b27fbb
Tom M <tom@collider.in>
2017-06-22 14:25:12 +0100

    Remove santise.py and make learn.py reorganize if possible

--- learn.py
+++ learn.py
@@ -3,7 +3,19 @@ import sys
 
 c = chatter.Chatter(sys.argv[1]+".d", writeback=True)
 
+print("Learning...")
 for l in open(sys.argv[1], 'r'):
 	c.learn(l)
+
+print("Writing to disk...")
+c.sync()
+
+# the seed db can get large, so shrink it
+db = c.seed.brain.dict
+if hasattr(db, "reorganize"):
+	print("Reorganizing...")
+	db.reorganize()
+else:
+	print("Skipped reorganizing (gdbm unavailable)")
 
 c.close()
--- sanitise.py
+++
@@ -1,4 +0,0 @@
-for l in open("messages.txt", 'r'):
-	l=l[2:]
-	n=len(l)
-	if n > 20 and n < 150: print(l, end="")
