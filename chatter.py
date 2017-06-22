eb07f95e2964c66a3e107d566a74ed7e853fa24e
Tom M <tom@collider.in>
2017-06-22 17:11:13 +0100

    Move chain class to a separate file

--- chatter.py
+++ chatter.py
@@ -1,49 +1,9 @@
 import random
-from itertools import count, chain
 import re
-import shelve
-from os import mkdir
 import os.path
-
-
-def _choices(choices):
-    """Choose an item randomly. choices is a sequence of (item, weight)"""
-    total = sum(w for c, w in choices)
-    r = random.uniform(0, total)
-    upto = 0
-    for c, w in choices:
-        if upto + w >= r:
-            return c
-        upto += w
-    assert False
-
-class MarkovChain():
-
-    def __init__(self, name, writeback=False):
-        self.brain = shelve.open(name, writeback=writeback)
-        self.states = self.brain.keys()
-
-    def observe(self, state, nextst):
-        links = self.brain.get(state, ([],[]) )
-        if nextst not in links[0]:
-            links[0].append(nextst)
-            links[1].append(1)
-        else:
-            pos = links[0].index(nextst)
-            links[1][pos] += 1
-        self.brain[state] = links
-
-    def findnext(self, state):
-        links = self.brain[state]
-        pairs = zip(*links)
-        return _choices(tuple(pairs))
-
-    def close(self):
-        self.brain.close()
-
-    def sync(self):
-        self.brain.sync()
-
+from itertools import count, chain
+from os import mkdir
+from markov import MarkovChain
 
 def _find_seq(chain, nms):
     """Continue the sentence from the two given norms.
---
+++ markov.py
@@ -0,0 +1,40 @@
+import random
+import shelve
+
+def _choices(choices):
+    """Choose an item randomly. choices is a sequence of (item, weight)"""
+    total = sum(w for c, w in choices)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in choices:
+        if upto + w >= r:
+            return c
+        upto += w
+    assert False
+
+class MarkovChain():
+
+    def __init__(self, name, writeback=False):
+        self.brain = shelve.open(name, writeback=writeback)
+        self.states = self.brain.keys()
+
+    def observe(self, state, nextst):
+        links = self.brain.get(state, ([],[]) )
+        if nextst not in links[0]:
+            links[0].append(nextst)
+            links[1].append(1)
+        else:
+            pos = links[0].index(nextst)
+            links[1][pos] += 1
+        self.brain[state] = links
+
+    def findnext(self, state):
+        links = self.brain[state]
+        pairs = zip(*links)
+        return _choices(tuple(pairs))
+
+    def close(self):
+        self.brain.close()
+
+    def sync(self):
+        self.brain.sync()
