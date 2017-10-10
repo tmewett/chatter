f1b882d90f14ca81e0fe6a4e18a64d0430eeb7b8
Tom M <tom@collider.in>
2017-10-10 11:46:54 +0100

    Speed up mass learning by circumventing shelf writeback/cache logic

--- utils.py
+++ utils.py
@@ -4,11 +4,24 @@ def learn(fn):
 	"""Learns each line of the file named *fn* into the Chatter DB 'fn.d'."""
 	c = Chatter(fn+".d", writeback=True)
 
+	# Learn directly to the shelf cache (a dict) for speed
+	for attr in "fore back case seed".split():
+		chain = getattr(c, attr)
+		chain._brain = chain.brain
+		chain.brain = chain.brain.cache
+
 	print("Learning...")
 	for l in open(fn, 'r'):
 		c.learn(l)
 
 	print("Writing to disk...")
+
+	# Replace shelf before we close
+	for attr in "fore back case seed".split():
+		chain = getattr(c, attr)
+		chain.brain = chain._brain
+		del chain._brain
+
 	c.sync()
 	c.close()
 
